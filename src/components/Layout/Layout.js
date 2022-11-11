e476967383bee53b87b259aa5fbc206a7024c4bc
Ejim Favour <favourejim56@gmail.com>
2022-11-11 13:40:42 +0100

    feat: react router installed

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import classes from "./Layout.module.scss";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Layout = (props) => {
   return (
     <>
       {/* header */}
-
+      <Header />
       {/* main */}
       <main>{props.children}</main>
       {/* footer */}
+      <Footer />
     </>
   );
 };
