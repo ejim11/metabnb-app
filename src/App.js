ccefc4795200016a9e82d1c2924cda5ea629d4fd
Ejim Favour <favourejim56@gmail.com>
2022-11-11 13:51:52 +0100

    feat: routes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Layout from "./components/Layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import PlaceToStay from "./Pages/PlaceToStay";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/place-to-stay" element={<PlaceToStay />} />
+        </Routes>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
