2fda81c64a2d9045e6ac11db260090f76f152110
Ejim Favour <favourejim56@gmail.com>
2022-11-11 17:03:25 +0100

    chore: added the header comp contents

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,52 @@
 import classes from "./Header.module.scss";
 import logoColored from "../../assets/logo-coloured.svg";
+import { NavLink } from "react-router-dom";
+import Button from "../UI/Button/Button";
+
+const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Place to stay",
+    path: "/place-to-stay",
+  },
+  {
+    title: "NFTs",
+    path: "/nfts",
+  },
+  {
+    title: "Community",
+    path: "/community",
+  },
+];
 
 const Header = () => {
-  return <header></header>;
+  const navLinks = links.map((link) => (
+    <li>
+      <NavLink
+        to={link.path}
+        className={(navData) =>
+          navData.isActive ? classes["active-link"] : classes["inactive-link"]
+        }
+      >
+        {link.title}
+      </NavLink>
+    </li>
+  ));
+
+  return (
+    <header>
+      <div className={classes.logo}>
+        <img src={logoColored} alt="logo" />
+      </div>
+      <nav>
+        <ul>{navLinks}</ul>
+      </nav>
+      <Button className={classes["connect-btn"]}>Connect wallet</Button>
+    </header>
+  );
 };
 
 export default Header;
