5a55055ef260aa820216a80b2260e4ae3536430f
Ejim Favour <favourejim56@gmail.com>
2022-11-13 00:02:40 +0100

    feat: sponsor images on home page

--- src/components/HomeComp/HomeComp.js
+++ src/components/HomeComp/HomeComp.js
@@ -1,9 +1,22 @@
+import mbTokenImg from "../../assets/mbToken.svg";
+import metaMaskImg from "../../assets/metamask-logo.svg";
+import openSeaImg from "../../assets/opensea.svg";
+import classes from "./HomeComp.module.scss";
 import FirstSection from "./FirstSection/FirstSection";
 
+const imgs = [mbTokenImg, metaMaskImg, openSeaImg];
+
 const HomeComp = () => {
+  const images = imgs.map((img, i) => (
+    <div key={i}>
+      <img src={img} alt="sponsor-img" />
+    </div>
+  ));
+
   return (
     <>
       <FirstSection />
+      <div className={classes["imgs-container"]}>{images}</div>
     </>
   );
 };
