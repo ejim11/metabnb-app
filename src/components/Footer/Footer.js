b9bb6bac397b318ee156c868460f098a45caa02f
Ejim Favour <favourejim56@gmail.com>
2022-11-12 09:18:06 +0100

    feat: footer contents created

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,42 @@
 import classes from "./Footer.module.scss";
+import metatbnbLogo from "../../assets/logo-white.svg";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+const footerLinksText = [
+  ["Community", "NFT", "Tokens", "Landlords", "Discord"],
+  ["Places", "Castle", "Farms", "Beach", "Learn more"],
+  ["About us", "Road map", "Creators", "Career", "Contact us"],
+];
 
 const Footer = () => {
-  return;
+  const footerLinks = footerLinksText.map((list) => (
+    <ul>
+      {list.map((text) => (
+        <li>
+          <a href="#">{text}</a>
+        </li>
+      ))}
+    </ul>
+  ));
+
+  return (
+    <footer>
+      <div className={classes["first-container"]}>
+        <div className={classes["logo-img"]}>
+          <img src={metatbnbLogo} alt="metabnb-logo" />
+        </div>
+        <div className={classes["media-links-icons"]}>
+          <FaFacebookF className={classes["icon"]} />
+          <FaInstagram className={classes["icon"]} />
+          <FaTwitter className={classes["icon"]} />
+        </div>
+      </div>
+      <div className={classes["ft-links-container"]}>{footerLinks}</div>
+      <p>© 2022 Metabnb</p>
+    </footer>
+  );
 };
 
 export default Footer;
