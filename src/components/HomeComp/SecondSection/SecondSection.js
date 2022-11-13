d3e3c37041613aa2b28b774c922a308bbdc8e8dc
Ejim Favour <favourejim56@gmail.com>
2022-11-13 01:01:18 +0100

    feat: second home section

---
+++ src/components/HomeComp/SecondSection/SecondSection.js
@@ -0,0 +1,12 @@
+import Card from "../../UI/Card/Card";
+import classes from "./SecondSection.module.scss";
+
+const SecondSection = () => {
+  return (
+    <Card>
+      <h2>Inspiration for your next adventure</h2>
+    </Card>
+  );
+};
+
+export default SecondSection;
---
+++ src/components/HomeComp/SecondSection/itemsData.js
@@ -0,0 +1,69 @@
+import image1 from "../../../assets/home-second-section/Frame 1.jpg";
+import image2 from "../../../assets/home-second-section/Frame 2.jpg";
+import image3 from "../../../assets/home-second-section/Frame 3.jpg";
+import image4 from "../../../assets/home-second-section/Frame 4.jpg";
+import image5 from "../../../assets/home-second-section/Frame 5.jpg";
+import image6 from "../../../assets/home-second-section/Frame 6.jpg";
+import image7 from "../../../assets/home-second-section/Frame 7.jpg";
+import image8 from "../../../assets/home-second-section/Frame 8.jpg";
+
+const itemsData = [
+  {
+    src: image1,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image2,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image3,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image4,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image5,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image6,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image7,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image8,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+];
+
+export default itemsData;
