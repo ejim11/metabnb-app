6ea23518e8bfa7f96047b0ef93e00c3d642cf8d9
Ejim Favour <favourejim56@gmail.com>
2022-11-15 06:19:09 +0100

    style: styled the img items

--- src/components/PlaceToStayComp/PlaceToStayComp.js
+++ src/components/PlaceToStayComp/PlaceToStayComp.js
@@ -1,10 +1,25 @@
 import FilterComp from "../FilterComp/FilterComp";
 import Card from "../UI/Card/Card";
+import itemsData from "./PlaceToStayData";
+import ImgItemBox from "../UI/ImgItemBox/ImgItemBox";
+import classes from "./PlaceToStayComp.module.scss";
 
 const PlaceToStayComp = () => {
+  const itemsList = itemsData.map((item, i) => (
+    <ImgItemBox
+      key={i}
+      src={item.src}
+      name={item.name}
+      cost={item.cost}
+      distance={item.distance}
+      duration={item.duration}
+    />
+  ));
+
   return (
     <Card>
       <FilterComp />
+      <div className={classes["items-list-container"]}>{itemsList}</div>
     </Card>
   );
 };
---
+++ src/components/PlaceToStayComp/PlaceToStayData.js
@@ -0,0 +1,133 @@
+import image1 from "../../assets/place to stay page images/items imgs/Frame 1.jpg";
+import image2 from "../../assets/place to stay page images/items imgs/Frame 2.jpg";
+import image3 from "../../assets/place to stay page images/items imgs/Frame 3.jpg";
+import image4 from "../../assets/place to stay page images/items imgs/Frame 4.jpg";
+import image5 from "../../assets/place to stay page images/items imgs/Frame 5.jpg";
+import image6 from "../../assets/place to stay page images/items imgs/Frame 6.jpg";
+import image7 from "../../assets/place to stay page images/items imgs/Frame 7.jpg";
+import image8 from "../../assets/place to stay page images/items imgs/Frame 8.jpg";
+import image9 from "../../assets/place to stay page images/items imgs/Frame 9.jpg";
+import image10 from "../../assets/place to stay page images/items imgs/Frame 10.jpg";
+import image11 from "../../assets/place to stay page images/items imgs/Frame 11.jpg";
+import image12 from "../../assets/place to stay page images/items imgs/Frame 12.jpg";
+import image13 from "../../assets/place to stay page images/items imgs/Frame 13.jpg";
+import image14 from "../../assets/place to stay page images/items imgs/Frame 14.jpg";
+import image15 from "../../assets/place to stay page images/items imgs/Frame 15.jpg";
+import image16 from "../../assets/place to stay page images/items imgs/Frame 16.jpg";
+
+const itemsData = [
+  {
+    src: image1,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image2,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image3,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image4,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image5,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image6,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image7,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image8,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image9,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image10,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image11,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image12,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image13,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image14,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image15,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+  {
+    src: image16,
+    name: "Desert King",
+    cost: "1MBT per night",
+    distance: "2345Km away",
+    duration: "available for 2 weeks stay",
+  },
+];
+
+export default itemsData;
